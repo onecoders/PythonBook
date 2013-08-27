1b7ed111142051e76d6358a9dba9bdf526c0df1a
silicon ruby <silicon@nachuantech.com>
2013-08-27 22:25:20 +0800

    Files and Exceptions

--- HeadFirstPython/chapter3/sketch_extra_logic.py
+++ HeadFirstPython/chapter3/sketch_extra_logic.py
@@ -1,17 +1,17 @@
-try:
+import os
+
+if os.path.exists('sketch.txt'):  
     data = open('sketch.txt')
 
     #when test the function in shell using as namespace, remember to invoke data.seek(0) to restart from first line
 
     for each_line in data:
-        try:
+        if not each_line.find(':') == -1:
             (role, line_spoken) = each_line.split(':', 1)
             print(role, end='')
             print(' said: ', end='')
             print(line_spoken, end='')
-        except ValueError:
-            pass
 
     data.close()
-except IOError:
+else:
     print('The data file is missing')
